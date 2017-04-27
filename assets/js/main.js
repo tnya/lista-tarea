99257a993892962a2510e435a3fc8b6063cea757
Tanya Ramirez <tcramire@gmail.com>
2017-04-27 14:06:49 -0300

    Tengo FOR que recorre arreglo para tomar 10 pero me falta unirlo con la funcion y corra

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,36 @@
-alert("lala");
+
+/* QUE TIENE QUE HACER MI PROGRAMA:
+
+- Mostrar las primeras 10 tareas solamente
+(ideas? hacer ciclo FOR para recorrer las primeras 10, guardarlas en "algun lugar" darles un id y ponerlas dentro de una lista en html)
+- Tareas deben ir en un arreglo y sumar las nuevas ingresadas 
+(ideas? ciclo FOR con contador que agregue tareas nuevas)
+*/
+
+/*PARA TENER LAS PRIMERAS 10 TAREAS:
+
+- Recorro array para buscar las primeras 10
+- .push para agregar cada una de las tareas al arreglo []
+*/
+
+
+
+
+
+//PARA AGREGAR TAREAS NUEVAS 
+
+function todoList(){
+	var task = document.getElementById("todoInput").value
+	var text = document.createTextNode(item)
+	var newItem = document.createElement("li")
+	newItem.appendChild(text)
+	document.getElementById("todoList").appendChild(newItem)
+
+}
+
+
+
+
+
+
+
